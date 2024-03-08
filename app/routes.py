f5014ff3fc0e6c419fd732e141f364f2033c5d79
htsx <joerodriguez7101998@gmail.com>
2024-03-08 14:56:01 -0500

    u

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from app import app
-from flask import Flask, render_template, redirect, url_for, request
-import sys
+from flask import Flask, render_template, redirect, request
 
 @app.route('/')
 def main():
@@ -12,6 +11,5 @@ def add():
         num1 = request.form.get('num1')
         num2 = request.form.get('num2')
         add = float(num1) + float(num2)
-        result = float(num1) + float(num2)
         return render_template('add.html', add=add)
     return render_template('homepage.html')
