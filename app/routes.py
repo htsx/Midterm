90f6f3a890f8296e345e9801cbf03db2552d1cf0
htsx <joerodriguez7101998@gmail.com>
2024-03-08 16:38:42 -0500

    u

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,21 @@ from flask import render_template, redirect, request
 def home():
     return render_template('homepage.html')
 
-@app.route('/add')
+@app.route('/add', methods==['GET', 'POST'])
 def add():
+    n1 = float(request.form['n1'])
+    n2 = float(request.form['n2'])
+    result = n1+n2
+    return render_template('add.html', result=result)
+
+@app.route('/add', methods==['GET', 'POST'])
+def sub():
+    return render_template('add.html')
+
+@app.route('/add', methods==['GET', 'POST'])
+def mult():
+    return render_template('add.html')
+
+@app.route('/add', methods==['GET', 'POST'])
+def div():
     return render_template('add.html')
