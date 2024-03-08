cb7e58d5a32372c9cc7c16911266ad713dc51939
htsx <joerodriguez7101998@gmail.com>
2024-03-08 18:44:58 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -11,3 +11,24 @@ def add():
     n2 = float(request.form['n2'])
     result = n1+n2
     return render_template('add.html', result=result)
+
+@app.route('/sub', methods=['POST'])
+def add():
+    n1 = float(request.form['n1'])
+    n2 = float(request.form['n2'])
+    result = n1+n2
+    return render_template('substract.html', result=result)
+
+@app.route('/mult', methods=['POST'])
+def add():
+    n1 = float(request.form['n1'])
+    n2 = float(request.form['n2'])
+    result = n1+n2
+    return render_template('multiply.html', result=result)
+
+@app.route('/div', methods=['POST'])
+def add():
+    n1 = float(request.form['n1'])
+    n2 = float(request.form['n2'])
+    result = n1+n2
+    return render_template('divide.html', result=result)
