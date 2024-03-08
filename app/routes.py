ebf11b087997cae9c833dc2bccded4d522bb8d3d
htsx <joerodriguez7101998@gmail.com>
2024-03-08 18:47:40 -0500

    u

--- app/routes.py
+++ app/routes.py
@@ -16,19 +16,19 @@ def add():
 def sub():
     n1 = float(request.form['n1'])
     n2 = float(request.form['n2'])
-    result = n1+n2
+    result = n1-n2
     return render_template('substract.html', result=result)
 
 @app.route('/mult', methods=['POST'])
 def mult():
     n1 = float(request.form['n1'])
     n2 = float(request.form['n2'])
-    result = n1+n2
+    result = n1*n2
     return render_template('multiply.html', result=result)
 
 @app.route('/div', methods=['POST'])
 def divide():
     n1 = float(request.form['n1'])
     n2 = float(request.form['n2'])
-    result = n1+n2
+    result = n1/n2
     return render_template('divide.html', result=result)
