b611a9db7fdc0199552af77e24e4e2303afad4d8
htsx <joerodriguez7101998@gmail.com>
2024-03-08 14:48:21 -0500

    u

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,8 @@ def main():
 @app.route('/', methods=['GET',"POST"])
 def add():
     if request.method == "POST":
-        num1 = request.form['num1']
-        num2 = request.form['num2']
+        num1 = request.form.get['num1']
+        num2 = request.form.get['num2']
         add = float(num1) + float(num2)
         result = float(num1) + float(num2)
         return render_template('add.html', add=add)
