8bdfdf3c738c67a558e0254451e25012304b7179
htsx <joerodriguez7101998@gmail.com>
2024-03-08 15:19:49 -0500

    u

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,11 @@
 from app import app
 from flask import Flask, render_template, redirect, request
 
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-    return render_template('homepage.html')
-
 @app.route('/', methods=['GET',"POST"])
 def add():
     if request.method == "POST":
-        num1 = request.form.get('num1')
-        num2 = request.form.get('num2')
+        n1 = request.form.get('num1')
+        n2 = request.form.get('num2')
         add = float(num1) + float(num2)
         return render_template('add.html', add=add)
     return render_template('homepage.html')
