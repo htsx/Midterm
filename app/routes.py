c43ee092d0a324ef8d2519c62e782eee87b93707
htsx <joerodriguez7101998@gmail.com>
2024-03-08 11:24:11 -0500

    updated

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,25 @@ from flask import render_template, redirect, url_for
 import sys
 
 @app.route('/')
-def hello():
+def main():
     return render_template('homepage.html')
+
+@app.route('/')
+def add():
+    return render_template('add.html')
+
+@app.route('/')
+def substract():
+    return render_template('substract.html')
+
+@app.route('/')
+def multiply():
+    return render_template('multiply.html')
+
+@app.route('/')
+def divide():
+    return render_template('divide.html')
+
+@app.route('/')
+def clear():
+    return render_template('clear.html')
