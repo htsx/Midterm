0213b5baf79a7e1f625e3e050721acb8985a85c6
htsx <joerodriguez7101998@gmail.com>
2024-03-08 14:57:47 -0500

    u

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import Flask, render_template, redirect, request
 
+app = Flask(__name__)
+
 @app.route('/')
 def main():
     return render_template('homepage.html')
