1078a7b6ff16896885255d6e7cc761853f5ffa7c
htsx <joerodriguez7101998@gmail.com>
2024-03-08 18:21:28 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request
 
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('homepage.html')
@@ -11,3 +13,6 @@ def add():
     n2 = float(request.form['n2'])
     result = n1+n2
     return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
